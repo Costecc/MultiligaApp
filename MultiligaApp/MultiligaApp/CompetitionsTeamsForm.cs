eae4008836112dbd3e3bcd78dd40b5ef4e0f2532
Jakub Kalinowski <kalina559@gmail.com>
2020-09-01 18:55:13 +0200

    database added

---
+++ MultiligaApp/MultiligaApp/CompetitionsTeamsForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiligaApp
+{
+    public partial class CompetitionsTeamsForm : Form
+    {
+        public CompetitionsTeamsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ReturnButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
