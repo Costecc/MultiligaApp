97c3369a0263d910db8edfdcf15708fb68d3ae2a
Jakub Kalinowski <kalina559@gmail.com>
2020-10-23 23:00:08 +0200

    added option to delete an account + minor changes

---
+++ MultiligaApp/CompetitionsForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiligaApp
+{
+    public partial class CompetitionsForm : Form
+    {
+        public CompetitionsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CompetitionsForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Application.OpenForms.Count == 0)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                int visibleForms = 0;
+                for (int i = 0; i < Application.OpenForms.Count; ++i)
+                {
+                    if (Application.OpenForms[i].Visible == true)
+                    {
+                        ++visibleForms;
+                    }
+                }
+                if (visibleForms == 0)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
---
+++ MultiligaApp/ProfileForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiligaApp
+{
+    public partial class ProfileForm : Form
+    {
+        public ProfileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void InviteButton_Click(object sender, EventArgs e)
+        {
+            if(InviteButton.Text == "Zaproś do drużyny")
+            {
+                //TODO - w miejsce if(true) zrobic ifa sprawdzajacego czy dany gracz juz jest w 
+                //przynajmniej jednej druzynie. Jesli jest to nie mozna zaprosic do druzyny
+                if(true)
+                    MessageBox.Show("Zaproszono do drużyny", "Zaproszenie wysłane");
+                else
+                    MessageBox.Show("Nie można zaprosić do drużyny! Zawodnik jest już w innej drużynie", "Informacja");
+            }
+            else
+            {
+                //TODO - w miejsce if(true) zrobic ifa sprawdzajacego czy dana drużyna juz jest w 
+                //przynajmniej jednych zawodach. Jesli jest to nie mozna zaprosic do zawodów
+
+                if(true)
+                    MessageBox.Show("Zaproszono do zawodów", "Zaproszenie wysłane");
+                else
+                    MessageBox.Show("Nie można zaprosić do zawodów! Drużyna jest już w innych zawodach", "Informacja");
+            }
+        }
+
+        private void ProfileForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (Application.OpenForms.Count == 0)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                int visibleForms = 0;
+                for (int i = 0; i < Application.OpenForms.Count; ++i)
+                {
+                    if (Application.OpenForms[i].Visible == true)
+                    {
+                        ++visibleForms;
+                    }
+                }
+                if (visibleForms == 0)
+                {
+                    Application.Exit();
+                }
+            }
+        }
+    }
+}
